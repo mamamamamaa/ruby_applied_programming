9ab38ed6e07e1a38dbf8c0abf9c27fac18d60ae0
Max Denyshchuk <maks.denishchuk@gmail.com>
2023-10-24 17:14:30 +0300

    fix up

--- run.rb
+++ run.rb
@@ -10,17 +10,17 @@ def run
     integer_value = input.to_i
     is_valid_integer = integer_value >= MIN_NUMBER && integer_value < MAX_NUMBER
 
-    puts is_valid_integer ?
-           "Roman number: #{integer_to_roman(integer_value)}" :
-           "Invalid integer number: (1 <= n < 4000)!"
+    puts !is_valid_integer ?
+           "Invalid integer number: (1 <= n < 4000)!" :
+           "Roman number: #{integer_to_roman(integer_value)}"
 
   elsif input.match?(ROMAN_REGEX)
     roman_value = input.upcase
     integer_result = roman_to_integer(roman_value)
 
-    puts integer_result ?
-           "Integer: #{integer_result}" :
-           "Invalid roman number!"
+    puts !integer_result ?
+           "Invalid roman number!" :
+           "Integer: #{integer_result}"
 
   else
     puts "Invalid input!"
