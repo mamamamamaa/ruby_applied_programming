1b77c5db846bf77fbddcf089a0e1e0f939a4271f
Max Denyshchuk <maks.denishchuk@gmail.com>
2023-10-24 17:26:19 +0300

    fix up

--- IPv4.rb
+++ IPv4.rb
@@ -6,13 +6,13 @@ def valid_ipv4?(ip)
 
   parts.all? do |part|
     is_number = part.match?(NUMBER_REGEX)
-    is_canonical = part.length > 1 ? part[0] != UNCONDITIONAL_SYMBOL : true
+    # is_canonical = part.length > 1 ? part[0] != UNCONDITIONAL_SYMBOL : true   # old
+    is_canonical = part == part.to_i.to_s #new
     is_valid_number = part.to_i.between?(MIN_NUMBER, MAX_NUMBER)
 
     is_number && is_canonical && is_valid_number
   end
 end
-
 
 puts valid_ipv4?('192.168.0.1')
 puts valid_ipv4?('256.256.256.256')
